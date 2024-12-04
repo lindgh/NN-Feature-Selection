7fdffb938746248cce090f5c939f9c8c52e0bf4f
Linda <lindaghun108@gmail.com>
2024-12-03 20:25:55 -0800

    validator class logic

---
+++ header/Validator.h
@@ -0,0 +1,35 @@
+#ifndef VALIDATOR
+#define VALIDATOR
+
+#include <iostream>
+#include <vector>
+
+#include "FeatureNode.h"
+
+using namespace std;
+
+class Validator
+{
+public:
+    // data members
+    vector<vector<float>> data;
+    vector<int> features;
+
+    // functions
+
+    // takes in file name, populates data with the stuff from filename
+    // should also do z score normalization on the data
+    //  -> (find mean of whole data set, find std dev, use to change every element)
+    Validator(string fileName);
+
+    // pass in a FeatureNode, updates validator's vector of features to FeatureNode's vector
+    void update_features(FeatureNode ft);
+
+    // takes in testSample, trainSample, and calcs distance using the features listed in FeatureNode features
+    float euclidean_distance(vector<float> testSample, vector<float> trainSample);
+
+    // returns accuracy of the classifier, given features store in class Validator
+    double NN_classifier();
+};
+
+#endif
