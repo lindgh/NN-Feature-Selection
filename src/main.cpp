bf9dced5abba1272076ecd1c7712b2b64e5af6a5
AKethidi <48158707+AKethidi@users.noreply.github.com>
2024-12-07 23:01:06 -0800

    fixed titanic results comment

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@
     18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 
     30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40}, Acc: 0.7220
 - Titanic Dataset Results:
-- Forward: Feature Subset: {2}, Acc: 0.7773
+- Forward: Feature Subset: {2,6}, Acc: 0.7773
 - Backward: Feature Subset: {1,2,3}, Acc: 0.7913
 
 */
